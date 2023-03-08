a94fa94f934c9102cf0a8a1616b157d43ae877de
Stephcyrille <stephcyril.sc@gmail.com>
2023-03-08 11:23:29 +0100

    Update skill block

--- src/component/gauge/CircularProgress.jsx
+++ src/component/gauge/CircularProgress.jsx
@@ -7,7 +7,7 @@ export const CircularProgress = (props) => {
     <>
       <CircularProgressbar 
         value={percent} 
-        text={`${percent}%`} 
+        text={`${percent}`} 
         styles={buildStyles({
           // Rotation of path and trail, in number of turns (0-1)
           rotation: 0.25,
@@ -16,7 +16,7 @@ export const CircularProgress = (props) => {
           strokeLinecap: 'butt',
       
           // Text size
-          textSize: '16px',
+          textSize: '13px',
       
           // How long animation takes to go from one percentage to another, in seconds
           pathTransitionDuration: 0.5,
@@ -31,7 +31,7 @@ export const CircularProgress = (props) => {
           backgroundColor: '#37d137',
         })}
       />
-      <div className='text-center py-2'>{ text }</div>
+      <div className='text-center py-1' style={{ fontSize: "12px" }}>{ text }</div>
     </>
   )
 }
--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -21,19 +21,20 @@ export const Skills = () => {
   return (
     <div className={ styles.sectionSkills }>
       <div className='row'>
-        <div className='col-md-12 col-12 mb-5'>
-          <h3 className="mt-0">Langage de Programmation</h3>
+        <div className='col-md-6 col-12 mb-5'>
+          <h3 className="mt-0">Programmation</h3>
           <div className='row'>
-            <LanguageLine language='HTML / CSS' />
-            <LanguageLine language='Javascript' />
             <LanguageLine language='Python' />
+            <LanguageLine language='R' />
+            <LanguageLine language='Javascript' />
+            <LanguageLine language='Typescript' />
             <LanguageLine language='DART' />
             <LanguageLine language='SQL' />
             <LanguageLine language='VBA' />
           </div>
         </div>
-        <div className='col-md-12 mb-2'>
-          <h3 className="mt-0">Compétences personnelles</h3>
+        <div className='col-md-6 mb-2'>
+          <h3 className="mt-0">Mes qualités</h3>
           <div className='row'>
             <div className='col-4 col-lg-3'>
                <CircularProgress text={"Discipliné"} percent={90} />
